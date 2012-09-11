c8f30e93079dc320abcd4eb2f029aadd0f6045e9
Steve Purcell <steve@sanityinc.com>
2012-09-11 09:18:46 +0200

    Remove URL from package description
    Embedding the URL in the package description makes for clunky package lists on Melpa and in `M-x package-list-packages`. This commit removes the URL, keeping with the convention of using a single short sentence for the package description.

--- tumblesocks-pkg.el
+++ tumblesocks-pkg.el
@@ -1,5 +1,5 @@
 (define-package "tumblesocks" "0.0.3"
-  "An Emacs tumblr client. See http://github.com/gcr/tumblesocks"
+  "An Emacs tumblr client"
   '((htmlize "1.39")
     (oauth "1.0.3")
     (markdown-mode "1.8.1")))
