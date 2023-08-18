1190772363fb0c06c53989f5ee2d32616444ac0b
Johan Laenen <johan.laenen+git@gmail.com>
2023-08-18 13:28:12 +0200

    key "a" will get us the list of notifications

--- tumblesocks-view.el
+++ tumblesocks-view.el
@@ -46,6 +46,7 @@ This causes Tumblesocks to ignore the setting of
     (define-key tumblesocks-view-mode-map "r" 'tumblesocks-view-reblog-post-at-point)
     (define-key tumblesocks-view-mode-map (kbd "RET") 'tumblesocks-view-post-at-point)
     (define-key tumblesocks-view-mode-map (kbd "SPC") 'forward-page)
+    (define-key tumblesocks-view-mode-map "a" 'tumblesocks-view-notifications)
     (define-key tumblesocks-view-mode-map "b" 'tumblesocks-view-blog)
     (define-key tumblesocks-view-mode-map "d" 'tumblesocks-view-delete-post-at-point)
     (define-key tumblesocks-view-mode-map "e" 'tumblesocks-view-edit-post-at-point)
@@ -53,7 +54,6 @@ This causes Tumblesocks to ignore the setting of
     (define-key tumblesocks-view-mode-map "l" 'tumblesocks-view-like-post-at-point)
     (define-key tumblesocks-view-mode-map "o" 'tumblesocks-view-post-url-at-point)
     (define-key tumblesocks-view-mode-map "y" 'tumblesocks-view-yank-post-url-at-point)
-    (define-key tumblesocks-view-mode-map "a" 'tumblesocks-view-notifications)
     tumblesocks-view-mode-map))
 
 (defun tumblesocks-view-compose-new-post ()
